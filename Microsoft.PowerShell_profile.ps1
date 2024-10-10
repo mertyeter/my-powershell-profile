024ed868b1dcc8625dc71ff29a1a9b0d4015284e
mertyeter <mertyeter@outlook.com>
2024-10-10 23:33:23 +0300

    Added new functions

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,73 +1,131 @@
-# Modülleri yükle
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
 
-# Oh My Posh temasını uygula
 oh-my-posh --init --shell pwsh --config "$env:USERPROFILE\AppData\Local\Programs\oh-my-posh\themes\cloud-native-azure.omp.json" | Invoke-Expression
 
-# PSReadLine ayarları
 Set-PSReadLineOption -PredictionSource History
-Set-PSReadLineOption -PredictionViewStyle InlineView
+Set-PSReadLineOption -PredictionViewStyle ListView
 Set-PSReadLineOption -EditMode Windows
 
-# Aliaslar
-Set-Alias -Name c -Value Clear-Host
-Set-Alias -Name d -Value docker
-Set-Alias -Name dc -Value "docker compose"
-Set-Alias -Name k -Value kubectl
-
-# Fonksiyonlar
+# Function to clean the project by deleting 'bin' and 'obj' directories recursively
 function cleanProject {
-    <#
-    .SYNOPSIS
-    Deletes bin and obj folders recursively.
-
-    .DESCRIPTION
-    This function deletes all bin and obj folders in the specified path. If -Force is not specified, it will ask for confirmation.
-
-    .PARAMETER Path
-    The root path to start cleaning. Defaults to the current directory.
-
-    .PARAMETER Force
-    Forces the deletion without asking for confirmation.
-
-    .EXAMPLE
-    cleanProject -Path "C:\MyProject" -Force
-    Deletes bin and obj folders in C:\MyProject without confirmation.
-
-    .EXAMPLE
-    cleanProject -Path "C:\MyProject"
-    Asks for confirmation before deleting bin and obj folders in C:\MyProject.
-    #>
-    param (
-        [string]$Path = ".",
-        [switch]$Force
-    )
-    $LogPath = "$env:USERPROFILE\cleanProject.log"
-    if ($Force -or (Read-Host "Are you sure you want to delete bin and obj folders in $Path? (Y/N)" -eq 'Y')) {
-        Get-ChildItem -Path $Path -Include bin, obj -Recurse | ForEach-Object {
-            Write-Host "DELETED: $($_.FullName)"
-            Remove-Item -Path $_.FullName -Force -Recurse
-            Add-Content -Path $LogPath -Value "DELETED: $($_.FullName) at $(Get-Date)"
-        }
+    Get-ChildItem .\ -include bin,obj -Recurse | ForEach-Object {
+        Write-Host 'DELETED' $_.FullName
+        Remove-Item $_.FullName -Force -Recurse
     }
 }
 
+# Function to build all .csproj files in the current directory and its subdirectories
+function buildAll {
+    $baseDir = (Get-Item -Path ".\" -Verbose).FullName
+    Write-Host ("Scanning *.csproj files in " + $baseDir)
+    $projectPaths = Get-ChildItem -Path $baseDir -Include *.csproj -Recurse
+    Write-Host ("Total found: " + $projectPaths.Count)
+
+    foreach ($projectPath in $projectPaths) {
+        Write-Host ("Building => " + $projectPath)
+        dotnet build $projectPath
+    }
+}
+
+# Function to restore the Windows image to the default state using DISM
 function dismRestore {
     DISM /Online /Cleanup-Image /RestoreHealth
 }
 
+# Function to get system errors from the last 12 hours
+function getSystemErrors {
+    Get-WinEvent -FilterHashtable @{ LogName = 'System'; StartTime = (Get-Date).AddHours(-12) } |
+    Where-Object { $_.LevelDisplayName -eq "Critical" -or $_.LevelDisplayName -eq "Error" } |
+    Format-List Id, LevelDisplayName, TimeCreated, Message
+}
+
+# Function to clear the PowerShell history
 function clearHistory {
-    ([Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory())
+    [Microsoft.PowerShell.PSConsoleReadLine]::ClearHistory()
 }
 
-function updateSystem {
-    Write-Host "Updating system..."
-    Start-Process "powershell" -ArgumentList "-NoProfile -ExecutionPolicy Bypass -Command Update-Help"
+# Function to set the Azure subscription based on the environment
+function setAzSubscription {
+    param (
+        [ValidateSet("dev", "test", "uat", "live")]
+        [string]$env
+    )
+
+    $subscriptionId = switch ($env) {
+        "dev" { "your-dev-subscription-id" }
+        "test" { "your-test-subscription-id" }
+        "uat" { "your-uat-subscription-id" }
+        "live" { "your-live-subscription-id" }
+    }
+
+    az account set --subscription $subscriptionId
 }
 
-# Fonksiyonlar için aliaslar
-Set-Item -Path alias:clean-project -Value cleanProject
-Set-Item -Path alias:dism-restore -Value dismRestore
-Set-Item -Path alias:clear-history -Value clearHistory
-Set-Item -Path alias:update-system -Value updateSystem
+# Function to list Azure SQL firewall rules
+function listAzSqlFirewallRule {
+    param (
+      [string]$resourceGroupName,
+      [string]$serverName
+    )
+
+    az sql server firewall-rule list -g $resourceGroupName -s $serverName -otable
+}
+
+# Function to add an Azure SQL firewall rule
+function addAzSqlFirewallRule {
+    param (
+        [string]$resourceGroupName,
+        [string]$serverName,
+        [string]$ruleName,
+        [string]$startIp,
+        [string]$endIp
+    )
+
+    az sql server firewall-rule create -g $resourceGroupName -s $serverName -n $ruleName --start-ip-address $startIp --end-ip-address $endIp
+}
+
+# Function to delete an Azure SQL firewall rule
+function deleteAzSqlFirewallRule {
+    param (
+        [string]$resourceGroupName,
+        [string]$serverName,
+        [string]$ruleName
+    )
+
+    az sql server firewall-rule delete -g $resourceGroupName -s $serverName -n $ruleName
+}
+
+# Function to align git branches
+function gitAlign {
+    git checkout main
+    git pull
+    git fetch --prune
+    git branch --v | Select-String -Pattern ".*\[gone\].*" | ForEach-Object { ($_ -split "\s+")[1] } | ForEach-Object { git branch -D $_ }
+}
+
+# Function to clean and build dotnet project
+function dotnetCleanAndBuild {
+    dotnet clean
+    dotnet build
+}
+
+Set-Alias -Name c -Value cls
+Set-Alias -Name clear-history -Value clearHistory
+
+Set-Alias -Name d -Value docker
+Set-Alias -Name dc -Value "docker compose"
+Set-Alias -Name k -Value kubectl
+
+Set-Alias -Name dism-restore -Value dismRestore
+Set-Alias -Name get-sys-errors -Value getSystemErrors
+
+Set-Alias -Name set-az-sub -Value setAzSubscription
+Set-Alias -Name azsql-list-rule -Value listAzSqlFirewallRule
+Set-Alias -Name azsql-add-rule -Value addAzSqlFirewallRule
+Set-Alias -Name azsql-delete-rule -Value deleteAzSqlFirewallRule
+
+Set-Alias -Name git-align -Value gitAlign
+Set-Alias -Name build-all -Value buildAll
+Set-Alias -Name clean-project -Value cleanProject
+Set-Alias -Name dotnet-rebuild -Value dotnetCleanAndBuild
--- README.md
+++ README.md
@@ -2,17 +2,25 @@
 My PowerShell profile including:
 
 1. PSReadLine settings to use the prompt history to make predictions on what you want to see next. You must run the command below on your terminal once.
-```
-Install-Module PSReadLine -AllowPrerelease -Force
-```
+    ```powershell
+    Install-Module PSReadLine -AllowPrerelease -Force
+    ```
 2. Aliases for the commands I use daily.
 
 3. Custom functions and their aliases.
-    - **Clean Project:** Cleans the project folder by removing the bin and obj folders.
-    - **DISM Restore:** Restores the Windows image to the default state.
-    - **Clear History:** Clears the PowerShell history.
+    - **Clean Project:** Cleans the project folder by removing the bin and obj folders. Alias: `clean-project`
+    - **Build All:** Builds all .csproj files in the current directory and its subdirectories. Alias: `build-all`
+    - **DISM Restore:** Restores the Windows image to the default state. Alias: `dism-restore`
+    - **Get System Errors:** Retrieves system errors from the last 12 hours. Alias: `get-sys-errors`
+    - **Clear History:** Clears the PowerShell history. Alias: `clear-history`
+    - **Set Azure Subscription:** Sets the Azure subscription based on the environment. Alias: `set-az-sub`
+    - **List Azure SQL Firewall Rule:** Lists Azure SQL firewall rules for a specified server. Alias: `azsql-list-rule`
+    - **Add Azure SQL Firewall Rule:** Adds an Azure SQL firewall rule. Alias: `azsql-add-rule`
+    - **Delete Azure SQL Firewall Rule:** Deletes an Azure SQL firewall rule. Alias: `azsql-delete-rule`
+    - **Git Align:** Aligns git branches by deleting branches that are gone from the remote. Alias: `git-align`
+    - **Clean and Build Dotnet Project:** Cleans and builds the dotnet project. Alias: `dotnet-rebuild`
 
 # Things to do before using this profile
 
 1. Set up PSReadline and oh-my-posh, please have a look at Scott Hanselman's great blogpost [here](https://www.hanselman.com/blog/my-ultimate-powershell-prompt-with-oh-my-posh-and-the-windows-terminal).
-2. Don't forget to replace `<username>` tag on oh-my-posh config. 
+2. Don't forget to replace `<username>` tag on oh-my-posh config.
